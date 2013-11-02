620196142fe80cdc566a38e84b5ae72e70c6f8a9
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-02 16:06:02 +0900

    indexビューへの補足をする

--- pina/urls.py
+++ pina/urls.py
@@ -1,9 +1,16 @@
-from django.conf.urls import patterns
+# coding: utf8
+
+from django.conf.urls import patterns, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # url(r'^$', 'pina.views.index', name='pina-index'),
+    url(r'^$', 'pina.views.index', name='pina-index'),
+    url(r'^products$', 'pina.views.product_list', name='pina-product-list'),
+    url(r'^products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
+        name='pina-product-detail'),
+    url(r'^products/register$', 'pina.views.product_register',
+        name='pina-product-register'),
 )
