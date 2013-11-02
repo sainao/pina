8506ee9b988c561299caaed2cb475af4b001398d
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-02 15:11:34 +0900

    indexビューを追加する

--- pina/urls.py
+++ pina/urls.py
@@ -1,17 +1,9 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pina.views.home', name='home'),
-    # url(r'^pina/', include('pina.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+urlpatterns = patterns(
+    '',
+    # url(r'^$', 'pina.views.index', name='pina-index'),
 )
---
+++ pina/view.py
@@ -0,0 +1,8 @@
+# coding: utf8
+
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'pina/index.html')
