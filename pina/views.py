e60b50c4d7989c4b1be9cf1b27900dd2dd9d60e0
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-04 00:07:13 +0900

    商品詳細ビューを作成する

--- pina/views.py
+++ pina/views.py
@@ -2,8 +2,18 @@
 
 from django.shortcuts import render
 
-# from .models import Product
+from .models import Product
 
 
 def index(request):
     return render(request, 'pina/index.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'pina/product_list.html', {'products': products})
+
+
+def product_detail(request, product_id):
+    product = Product.objects.get(pk=product_id)
+    return render(request, 'pina/product_detail.html', {'product': product})
