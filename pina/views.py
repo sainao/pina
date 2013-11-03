cc2475e0fce0421155632a722db762b0026be4c8
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-03 22:21:35 +0900

    不要なimportをコメントアウトする

--- pina/views.py
+++ pina/views.py
@@ -1,13 +1,14 @@
 # coding: utf8
 
 from django.shortcuts import render
-from django.template import Context, loader
+# from django.template import Context, loader
 
 from .models import Product
 
 
 def index(request):
     return render(request, 'pina/index.html')
+
 
 def product_list(request):
     products = Product.objects.all()
