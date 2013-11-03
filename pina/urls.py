d27ad6fc7f7b6506096a03f1c68614e28ac7daa5
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-03 19:56:21 +0900

    商品一覧を表示するurls.py, views.py, product_list.html を作成する

--- pina/urls.py
+++ pina/urls.py
@@ -7,11 +7,11 @@ admin.autodiscover()
 
 
 urlpatterns = patterns(
-    '',
-    url(r'^$', 'pina.views.index', name='pina-index'),
-    # url(r'^products$', 'pina.views.product_list', name='pina-product-list'),
+    'pina.views',
+    url(r'^$', 'index', name='pina-index'),
+    url(r'^products$', 'product_list', name='pina-product-list'),
     # url(r'^products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
     #    name='pina-product-detail'),
-    #url(r'^products/register$', 'pina.views.product_register',
+    # url(r'^products/register$', 'pina.views.product_register',
     #    name='pina-product-register'),
 )
