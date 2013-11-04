3c6731d88f40a5ac65d4f32363e944a70131e88f
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-04 19:07:54 +0900

    商品一覧を表示するためにurls.pyファイルで必要な文のコメントアウトとviewsへの参照記述も変更する

--- pina/urls.py
+++ pina/urls.py
@@ -7,11 +7,12 @@ admin.autodiscover()
 
 
 urlpatterns = patterns(
-    'pina.views',
-    url(r'^$', 'index', name='pina-index'),
-    url(r'^pina/products$', 'product_list', name='pina-product-list'),
-    # url(r'^products/(?P<product_id>\d+)/$', 'product_detail',
+    '',
+    url(r'^$', 'pina.views.index', name='pina-index'),
+    url(r'^pina/products/$', 'pina.views.product_list',
+        name='pina-product-list'),
+    url(r'^products/(?P<product_id>\d+)/$', 'product_detail',
         name='pina-product-detail'),
     # url(r'^pina/products/register$', 'pina.views.product_register',
-        name='pina-product-register'),
+    #   name='pina-product-register'),
 )
