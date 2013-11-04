c89ed8c67779e6db292faa59a932f12de7e0d326
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-04 20:39:43 +0900

    商品詳細を表示するために修正する

--- pina/urls.py
+++ pina/urls.py
@@ -9,8 +9,9 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'pina.views.index', name='pina-index'),
-    url(r'^products$', 'pina.views.product_list', name='pina-product-list'),
-    url(r'^products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
+    url(r'^pina/products/$', 'pina.views.product_list',
+        name='pina-product-list'),
+    url(r'^pina/products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
         name='pina-product-detail'),
     #url(r'^products/register$', 'pina.views.product_register',
     #    name='pina-product-register'),
