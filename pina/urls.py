de65c296c8dd86acd40767d71165dac223cf52cb
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-04 17:45:53 +0900

    商品一覧リスト表示のurl.pyファイルを修正する

--- pina/urls.py
+++ pina/urls.py
@@ -9,9 +9,9 @@ admin.autodiscover()
 urlpatterns = patterns(
     'pina.views',
     url(r'^$', 'index', name='pina-index'),
-    url(r'^products/$', 'product_list', name='pina-product-list'),
-    url(r'^products/(?P<product_id>\d+)/$', 'product_detail',
+    url(r'^pina/products$', 'product_list', name='pina-product-list'),
+    # url(r'^products/(?P<product_id>\d+)/$', 'product_detail',
         name='pina-product-detail'),
-    # url(r'^products/register$', 'pina.views.product_register',
-    #    name='pina-product-register'),
+    # url(r'^pina/products/register$', 'pina.views.product_register',
+        name='pina-product-register'),
 )
