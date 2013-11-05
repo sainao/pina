1d42739cfb083c2939515aba0cd709ce72c70f11
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-05 23:47:59 +0900

    商品削除に関するビューの修正をする

--- pina/urls.py
+++ pina/urls.py
@@ -9,12 +9,21 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'pina.views.index', name='pina-index'),
+
     url(r'^pina/products/$', 'pina.views.product_list',
         name='pina-product-list'),
+
     url(r'^pina/products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
         name='pina-product-detail'),
-    url(r'^pina/products/(?P<product_id>\d+)/delete/$', 'pina.views.product_delete',
+
+    url(r'^pina/products/(?P<product_id>\d+)/check$',
+        'pina.views.product_delete_check',
+        name='pina-product-delete-check'),
+
+    url(r'^pina/products/(?P<product_id>\d+)/delete$',
+        'pina.views.product_delete',
         name='pina-product-delete'),
+
     # url(r'^pina/products/register$', 'pina.views.product_register',
     #   name='pina-product-register'),
 )
