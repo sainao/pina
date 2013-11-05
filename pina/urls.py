431752de99b18e01a3341b85f1698ed1429dcb64
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-05 12:49:41 +0900

    コンフリクト修正の際に、product-detail-viewの最終内容が反映できてなかったため同じに修正する

--- pina/urls.py
+++ pina/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns(
     url(r'^$', 'pina.views.index', name='pina-index'),
     url(r'^pina/products/$', 'pina.views.product_list',
         name='pina-product-list'),
-    url(r'^products/(?P<product_id>\d+)/$', 'product_detail',
+    url(r'^products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
         name='pina-product-detail'),
     # url(r'^pina/products/register$', 'pina.views.product_register',
     #   name='pina-product-register'),
