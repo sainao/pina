481d70419b04990453bbc526030fd878f45ed717
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-05 12:39:17 +0900

    コンクリフトが発生したので残す分だけにし修正する

--- pina/views.py
+++ pina/views.py
@@ -12,13 +12,9 @@ def index(request):
 
 def product_list(request):
     products = Product.objects.all()
-<<<<<<< HEAD
-    return render(request, 'pina/product_list.html', {"products": products})
-=======
     return render(request, 'pina/product_list.html', {'products': products})
 
 
 def product_detail(request, product_id):
     product = Product.objects.get(pk=product_id)
     return render(request, 'pina/product_detail.html', {'product': product})
->>>>>>> feature/add-product-detail-view
