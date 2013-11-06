51fe296b3edcd0f9d4b3848071bf57b1ac07e81f
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-06 18:47:32 +0900

    urlの表記を長くしていたので短縮する

--- pina/urls.py
+++ pina/urls.py
@@ -10,20 +10,20 @@ urlpatterns = patterns(
     '',
     url(r'^$', 'pina.views.index', name='pina-index'),
 
-    url(r'^pina/products/$', 'pina.views.product_list',
+    url(r'^products$', 'pina.views.product_list',
         name='pina-product-list'),
 
-    url(r'^pina/products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
+    url(r'^products/(?P<product_id>\d+)/$', 'pina.views.product_detail',
         name='pina-product-detail'),
 
-    url(r'^pina/products/(?P<product_id>\d+)/check$',
+    url(r'^products/(?P<product_id>\d+)/check$',
         'pina.views.product_delete_check',
         name='pina-product-delete-check'),
 
-    url(r'^pina/products/(?P<product_id>\d+)/delete$',
+    url(r'^products/(?P<product_id>\d+)/delete$',
         'pina.views.product_delete',
         name='pina-product-delete'),
 
-    # url(r'^pina/products/register$', 'pina.views.product_register',
-    #   name='pina-product-register'),
+    # url(r'^products/register$', 'pina.views.product_register',
+    #    name='pina-product-register'),
 )
