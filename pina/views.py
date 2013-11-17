2e0f9545187ce8e835d7877966dfa3280deec4ac
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-17 13:14:21 +0900

    未記入エラーの際に入力データを渡すページに飛ぶように修正する

--- pina/views.py
+++ pina/views.py
@@ -2,7 +2,6 @@
 
 from django.shortcuts import render
 from pina.forms import ProductForm
-# from django.template import Context, loader
 from django.http import Http404
 from django.http import HttpResponseRedirect
 
@@ -49,33 +48,32 @@ def product_delete(request, product_id):
     return render(
         request, 'pina/product_delete.html')
 
-    
+
 def product_entry(request):
     form = ProductForm()
-    return render(request, 'pina/product_entry.html',
-    {'form': form})
+    return render(
+        request, 'pina/product_entry.html',
+        {'form': form})
+
 
 def product_entry_confirm(request):
-    if request.method == 'POST': # フォームが提出された
-        form = ProductForm(request.POST) # POST データの束縛フォームの生成
-        if form.is_valid(): # バリデーション（入力検証）を通った
+    if request.method == 'POST':  # フォームが提出された
+        form = ProductForm(request.POST)  # POST データの束縛フォームの生成
+        if form.is_valid():  # バリデーション（入力検証）を通った
             t = form.cleaned_data['title']
             c = form.cleaned_data['content']
             p = form.cleaned_data['price']
 
-            from datetime import datetime
-            from .models import Product
-            # created_at = models.DateTimeField(auto_now_add=True)
-            # updated_at = models.DateTimeField(auto_now=True)
-            p = Product(title=t, content=c, price=p, created_at="", updated_at="")
+            p = Product(
+                title=t, content=c, price=p, created_at="", updated_at="")
             p.save()
-            return HttpResponseRedirect('completion') # POST 後のリダイレクト
+            # 入力検証が済み、作成日時や更新日時を保存し、完了ページへ移動する
+            return HttpResponseRedirect('completion')  # POST 後のリダイレクト
     else:
-        form = ProductForm() # 非束縛フォーム
-    
-    return render(request, 'pina/product_entry.html', {'form': form})
+        form = ProductForm()  # 非束縛フォーム
+    # 未記入がある際に入力された中途半端なデータを~confirm.htmlへ渡す
+    return render(request, 'pina/product_entry_confirm.html', {'form': form})
+
 
 def product_entry_completion(request):
     return render(request, 'pina/product_entry_completion.html')
-    
-  
