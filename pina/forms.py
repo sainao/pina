a2bb3c29c06f86f640f09c5f0af1012ac526a593
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-19 17:18:01 +0900

    foms.pyファイルにlabelの指定をし、context(説明)の引数に大きめウィジェットTextAreaを指定する

--- pina/forms.py
+++ pina/forms.py
@@ -3,6 +3,6 @@ from django import forms
 
 
 class ProductForm(forms.Form):
-    title = forms.CharField(max_length=100)
-    content = forms.CharField(max_length=300)
+    title = forms.CharField(max_length=300, label=u'商品名')
+    content = forms.CharField(widget=forms.Textarea)
     price = forms.IntegerField()
