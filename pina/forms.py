e2bca27488826b13204f5a8ec625baf866506a4d
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-23 20:41:14 +0900

    商品登録機能を追加する

---
+++ pina/forms.py
@@ -0,0 +1,16 @@
+# coding: utf8
+
+from django import forms
+from django.forms import ModelForm
+
+from .models import Product
+
+
+class ProductForm(ModelForm):
+    title = forms.CharField(label=u'商品名', max_length=10)
+    content = forms.CharField(label=u'説明', widget=forms.Textarea)
+    price = forms.IntegerField(label=u'価格')
+
+    class Meta:
+        model = Product
+        fields = ('title', 'content', 'price')
--- pina/views.py
+++ pina/views.py
@@ -1,9 +1,9 @@
 # coding: utf8
 
-from django.shortcuts import render
-# from django.template import Context, loader
+from django.shortcuts import render, redirect
 from django.http import Http404
 
+from .forms import ProductForm
 from .models import Product
 
 
@@ -46,3 +46,29 @@ def product_delete(request, product_id):
         raise Http404
     return render(
         request, 'pina/product_delete.html')
+
+
+def product_entry(request):
+    if request.method == 'POST':
+        # POST データの束縛フォームの生成
+        form = ProductForm(request.POST)
+        # バリデーション（入力検証）通過
+        if form.is_valid():
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            price = form.cleaned_data['price']
+
+            product = Product(
+                title=title, content=content, price=price,
+                created_at="", updated_at="")
+            product.save()
+            # POST 後のリダイレクト
+            return redirect('pina-product-entry-completion')
+    else:
+        # 非束縛フォーム生成（データが結びついてないフォーム)
+        form = ProductForm()
+    return render(request, 'pina/product_entry.html', {'form': form})
+
+
+def product_entry_completion(request):
+    return render(request, 'pina/product_entry_completion.html')
