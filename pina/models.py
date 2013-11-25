74866b1c2e1f3b0f5fbfc64c253f52ac87722a38
SAITO Naoko <korokoronaoko@gmail.com>
2013-11-26 04:23:06 +0900

    labelを追記する

--- pina/models.py
+++ pina/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(max_length=300)
-    price = models.IntegerField()
+    title = models.CharField(u'商品名', max_length=100)
+    content = models.TextField(u'説明')
+    price = models.IntegerField(u'価格')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
